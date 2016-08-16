ff762fe963d591b719dbc4ac82a2bfb0913e5e59
Lukas Klimmasch <klimmasch@fias.uni-frankfurt.de>
2016-08-16 16:18:18 +0200

    updated the plotting of trajectories to display three starting points. completePlotting was also slightly updated

--- completePlotting.m
+++ completePlotting.m
@@ -1,35 +1,22 @@
 % This script takes some files and generated the plots and makes some testing on them
-function completePlotting(reinitRenderer)
+function completePlotting()
     files = {...
-        '/home/klimmasch/projects/results/model_20-May-2016_13:10:14_500000_nonhomeo_1_2m_newImplem_highResSmSc_noMF/model.mat', ...
-        '/home/klimmasch/projects/results/model_21-May-2016_19:37:02_500000_nonhomeo_1_2m_newImplem_newSmallScale_1pcMF/model.mat', ...
-        '/home/klimmasch/projects/results/model_21-May-2016_19:37:45_500000_nonhomeo_1_2m_newImplem_newSmallScale_5pcMF/model.mat', ...
-        '/home/klimmasch/projects/results/model_21-May-2016_19:38:32_500000_nonhomeo_1_2m_newImplem_newSmallScale_10pcMF/model.mat',...
-        '/home/klimmasch/projects/results/model_20-May-2016_13:04:15_500000_nonhomeo_1_2m_newImplem_highRes-halfSize-SmallScale/model.mat',...
-        '/home/klimmasch/projects/results/model_21-May-2016_19:39:29_500000_nonhomeo_1_2m_newImplem_newSmallScale_50pcMF/model.mat', ...
-%         '/home/klimmasch/projects/results/model_23-May-2016_21:11:52_500000_nonhomeo_1_2m_newImplem_oldScalesHalfStride_0pcMF/model.mat', ...
-%         '/home/klimmasch/projects/results/model_23-May-2016_11:21:31_500000_nonhomeo_1_2m_newImplem_oldScalesHalfStride_1pcMF/model.mat', ...
-%         '/home/klimmasch/projects/results/model_23-May-2016_22:03:15_500000_nonhomeo_1_2m_newImplem_oldScalesHalfStride_5pcMF/model.mat', ...
-%         '/home/klimmasch/projects/results/model_23-May-2016_11:20:48_500000_nonhomeo_1_2m_newImplem_oldScaleHalfStride_10pcMF/model.mat',...
-%         '/home/klimmasch/projects/results/model_23-May-2016_11:25:30_500000_nonhomeo_1_2m_newImplem_oldScalesHalfStride_30pcMF/model.mat',...
-%         '/home/klimmasch/projects/results/model_23-May-2016_11:19:32_500000_nonhomeo_1_2m_newImplem_oldScales_50pcMF/model.mat', ...
-    %     '/home/lelais/Documents/MATLAB/results/model_20-Apr-2016_20:55:52_500000_nonhomeo_1_CACLAVarLu_stdR_var_10-5_lMFB_0.3_LR_1/model.mat', ...
-    %     '/home/lelais/Documents/MATLAB/results/model_20-Apr-2016_21:00:24_500000_nonhomeo_1_CACLAVarLu_stdR_var_10-4_lMFB_0.3_LR_1/model.mat', ...
-    %     '/home/lelais/Documents/MATLAB/results/model_19-Apr-2016_18:06:46_500000_nonhomeo_1_CACLAVarLu_stdR_var_10-5_lMFB_0.3_LR_1_winit_1_0.01_2/model.mat', ...
-    %     '/home/lelais/Documents/MATLAB/results/model_19-Apr-2016_18:09:26_500000_nonhomeo_1_CACLAVarBp_stdR_var_10-5_lMFB_0.3_LR_1_winit_1_0.01_2/model.mat', ...
-    %     '/home/lelais/Documents/MATLAB/results/model_20-Apr-2016_19:19:19_500000_nonhomeo_1_CACLAVarLu_stdR_var_10-5_lMFB_0.3_LR_1_winit_1_0.01_2_posActorInit/model.mat'};
-        %,...
-        %'/model.mat', ...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_20:05:06_3000000_nonhomeo_1_bmsf_newInit_steeperReward/model.mat', ...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_19:42:43_3000000_nonhomeo_1_bmsf_newInit_20iters/model.mat', ...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_19:46:26_3000000_nonhomeo_1_bmsf_newInit_metCost50pc/model.mat', ...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_19:47:48_3000000_nonhomeo_1_bmsf_newInit_metCost25pc/model.mat',...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_20:10:36_3000000_nonhomeo_1_bmsf_newInit_newRenderen_newImages/model.mat',...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_19:39:30_3000000_nonhomeo_1_bmsf_newInit_lMF1/model.mat', ...
+        '/home/klimmasch/projects/results/model_12-Aug-2016_19:36:14_3000000_nonhomeo_1_bmsf_newInit_400BF/model.mat', ...
+        '/home/klimmasch/projects/results/model_13-Aug-2016_03:55:38_3000000_nonhomeo_1_bmsf_newInit_newRender_InitNoReset/model.mat', ...
         };
 
     simulator = OpenEyeSim('create');
-    if reinitRenderer
-        simulator.reinitRenderer();
-    else
-        simulator.initRenderer();
-    end
+%     simulator = OpenEyeSimV4('create');
+%     simulator.reinitRenderer();
+    simulator.initRenderer();
     
-    stimulusRange = 33;
+    stimulusRange = 50;
     vergRange = 7;
     objDists = [0.5:0.5:2];
     objRange = length(objDists);
@@ -38,15 +25,21 @@ function completePlotting(reinitRenderer)
     imageFlag = sprintf('last%dvalues_newScale', takeLastValues);
 
     meanOffset = zeros(objRange, length(files), stimulusRange * takeLastValues);
+    
     for i = 1:length(files)
         model = load(files{i});
         model = model.model;
 %         model.allPlotSave;
 %         testModel(model, 23, [0.5, 1, 1.5, 2], [-2 : 0.2 : 2], [20, 20], 1, 0, 1, 1);
-        testModel2(model, 50, 1, 1, simulator, 0); % use more stimuli than in the textures file, just in case
-
-%         sprintf('######plotting and testing completed in %s #######', files{i});
-%         close all;
+%         testModel2(model, 50, 1, 1, simulator, 0); % use more stimuli than in the textures file, just in case
+        if i == 1
+            testModelContinuous(model, stimulusRange, 1, 1, simulator, 0, sprintf('modelAt%d,oldRenderer', model.trainedUntil));
+        else
+            testModelContinuous(model, stimulusRange, 1, 1, simulator, 1, sprintf('modelAt%d,oldRenderer', model.trainedUntil));
+        end
+        sprintf('###### plotting and testing completed in %s #######', files{i});
+        close all;
+        
 %         nSamples = size(model.testResult3, 1);
 %         if nSamples == 0
 %             simulator = OpenEyeSim('create');
@@ -54,39 +47,39 @@ function completePlotting(reinitRenderer)
 %             simulator.reinitRenderer();
 %             testModel2(model, 50, 0, 1, simulator, 0);
 %         end
-        nSamples = size(model.testResult3, 1)
-        if nSamples == 0
-            testModel2(model, 50, 0, 1, simulator, 0);
-        end
+%         nSamples = size(model.testResult3, 1)
+%         if nSamples == 0
+%             testModel2(model, 50, 0, 1, simulator, 0);
+%         end
         % extract all values that are generated with zero vergErr init
-        tmpResult = []; % these are the values of the 33 input stimuli that started with 0 vergErr at the end of 10 iterations
-        for j = 1:nSamples
+%         tmpResult = []; % these are the values of the 33 input stimuli that started with 0 vergErr at the end of 10 iterations
+%         for j = 1:nSamples
 
 
-            if mod(j, vergRange) == zeroVergInd % if we started at zero vergence Error
-                % append the last value(s)
-                for v = 1:takeLastValues
-                    tmpResult = [tmpResult, model.testResult3(j, end - v + 1)];
-                end
-            end
-
-            if length(tmpResult) == stimulusRange * takeLastValues
-                objInd = ceil((j * objRange) / nSamples);
-                meanOffset(objInd, i, :) = tmpResult;
-                tmpResult = [];
-            end
-        end
+%             if mod(j, vergRange) == zeroVergInd % if we started at zero vergence Error
+%                 % append the last value(s)
+%                 for v = 1:takeLastValues
+%                     tmpResult = [tmpResult, model.testResult3(j, end - v + 1)];
+%                 end
+%             end
+% 
+%             if length(tmpResult) == stimulusRange * takeLastValues
+%                 objInd = ceil((j * objRange) / nSamples);
+%                 meanOffset(objInd, i, :) = tmpResult;
+%                 tmpResult = [];
+%             end
+%         end
     end
-    %% todo: remove outliers from the plot and scale vergence error axis to the same range
-    for k = 1:objRange
-        figure; hold on; grid on; grid minor;
-        [~, tmpSize1, tmpSize2] = size(meanOffset(k, :, :));
-        boxplot(reshape(meanOffset(k, :, :), [tmpSize1, tmpSize2])', 'labels', [0 1 5 10 30 50]);
-        title(sprintf('Offset from zero vergence error at %.1f m', objDists(k)));
-        xlabel('lambda_{MF} in %');
-        ylabel('Vergence Error in deg');
-        ylim([-1, 2]);
-        saveas(gcf, sprintf('../results/biasesAt%.2f_%s.png', objDists(k), imageFlag), 'png');
-
-    end
+    %% todo: remove outliers from the plot and scale vergence error axis to the same range --> this is now part of the testmodelContinuous
+%     for k = 1:objRange
+%         figure; hold on; grid on; grid minor;
+%         [~, tmpSize1, tmpSize2] = size(meanOffset(k, :, :));
+%         boxplot(reshape(meanOffset(k, :, :), [tmpSize1, tmpSize2])', 'labels', [0 1 5 10 30 50]);
+%         title(sprintf('Offset from zero vergence error at %.1f m', objDists(k)));
+%         xlabel('lambda_{MF} in %');
+%         ylabel('Vergence Error in deg');
+%         ylim([-1, 2]);
+%         saveas(gcf, sprintf('../results/biasesAt%.2f_%s.png', objDists(k), imageFlag), 'png');
+% 
+%     end
 end
--- plotTrajectory.m
+++ plotTrajectory.m
@@ -1,14 +1,32 @@
 %%% This method creates a trajectory from the given paramters and plots it
 %%% on the plane of object depth.
 %%% Note that this script is intended solely for continuous models.
+% @param model                  the model to be tested
+% @param objDist                the object distance
+% @param startVergErr           the vergence error to muscles start with
+% @param initMethod             either 'simple' or 'random'
+% @paramnumIters                number of iterations that are executed
+% @param stimuli                an array of indizes from the texture files
+% @param simulator              either a simulator object or [] for a new one
+% @param reinit                 1 for reinit renderer, 0 for new initialization (for testing purposes)
+% @param savePlot               
+%%%
+function plotTrajectory(model, objDist, startVergErr, initMethod, numIters, stimuli, simulator, reinit, titleStr)
 
-function plotTrajectory(model, objDist, startVergErr, initMethod, numIters, stimuli, simulator, reinit, savePlot)
-
+    rng(1);
+    angleDes = atand(model.baseline / (2 * objDist)); % only for one eye this time
+    plotAnaglyphs = true;
+%     plotAnaglyphs = false;
+    
     %% everything concerning rendering:
 %     texture = load(['config/' 'Textures_mcGillTest']);
     texture = load(['config/' 'Textures_vanHaterenTest']);
     texture = texture.texture;
-    if length(stimuli) > length(texture)
+    nTextures = length(texture);
+    nStimuli = length(stimuli);
+    
+    cmdInit = [[0.003; 0.012], [0.003; 0.004], [0.01; 0.004]];
+    if length(stimuli) > nTextures
         sprintf('Texture file does not contain that many images, but I will continue anyways.')
         stimuli = stimuli(1:length(texture));
     end
@@ -114,23 +132,41 @@ function plotTrajectory(model, objDist, startVergErr, initMethod, numIters, stim
     
     %% main loop:
     trajectory = zeros(length(stimuli), numIters, 2);
-    for stim = 1:length(stimuli)
+    figure;
+%     title(sprintf('Trajectory of Oject Fixation at %1.1fm\n%s', objDist, titleStr));
+    figIter = 1;
+    for stim = 1:nStimuli
         currentTexture = texture{stimuli(stim)};
         
-        if initMethod == 'simple'
+        if strcmp(initMethod, 'simple')
             [command, angleNew] = getMF2(objDist, startVergErr);
-        elseif initMethod == 'random'
+            angleNew = angleNew / 2;
+        elseif strcmp(initMethod, 'random')
 %             initFixation = model.objDistMin + (model.objDistMax - model.objDistMin) * rand(1,1)
-            command = [0; 0];
-            command(1) = model.muscleInitMin(1) + (model.muscleInitMax(1) - model.muscleInitMin(1)) * rand(1, 1);
-            command(2) = model.muscleInitMin(2) + (model.muscleInitMax(2) - model.muscleInitMin(2)) * rand(1, 1); 
-            angleNew = getAngle(command) * 2;
+%             command = [0; 0];
+%             command(1) = model.muscleInitMin(1) + (model.muscleInitMax(1) - model.muscleInitMin(1)) * rand(1, 1);
+%             command(2) = model.muscleInitMin(2) + (model.muscleInitMax(2) - model.muscleInitMin(2)) * rand(1, 1); 
+            command = cmdInit(:, stim);
+            angleNew = getAngle(command);
         end
             
         for iter = 1:numIters
             trajectory(stim, iter, :) = command;
-            refreshImages(currentTexture, angleNew / 2, objDist, 3)
-
+            refreshImages(currentTexture, angleNew, objDist, 3)
+            
+            % show anaglyphs for quit performance check
+            if (plotAnaglyphs) && ((iter == 1) || (iter == numIters))
+                subplot(nStimuli, 2, figIter);
+                imshow(stereoAnaglyph(imgGrayLeft, imgGrayRight))
+                title(sprintf('fix. depth = %1.1fm (%.3f°)\nverg. error = %.3f', (model.baseline / (2 * tand(angleNew))), angleNew, angleDes - angleNew)) 
+                figIter = figIter + 1;
+%             elseif (plotAnaglyphs) && (iter == numIters)
+%                 subplot(nStimuli, 2, figIter);
+%                 imshow(stereoAnaglyph(imgGrayLeft, imgGrayRight))
+%                 title(sprintf('angle = %1.1fm (%.3f°),\nverg. error = %.3f', (model.baseline / (2 * tand(angleNew))), angleNew, angleDes - angleNew))   
+%                 figIter = figIter + 1;
+            end
+            
             for i = 1 : length(model.scModel)
                 model.preprocessImage(imgGrayLeft, i, 1);
                 model.preprocessImage(imgGrayRight, i, 2);
@@ -141,25 +177,46 @@ function plotTrajectory(model, objDist, startVergErr, initMethod, numIters, stim
             feature = [bfFeature; command * model.lambdaMuscleFB];          % append muscle activities to feature vector
             relativeCommand = model.rlModel.act(feature);                   % generate change in muscle activity
             command = checkCmd(command + relativeCommand);                  % calculate new muscle activities
-            angleNew = getAngle(command) * 2;                               % transform into angle
+            angleNew = getAngle(command);                               % transform into angle
+            
+            
         end
     end
     
     %% plotting results
-    figure;
+%     figure('size', [500, 500]);
+    figure();
     hold on;
+    title(sprintf('Oject Fixation at %1.1fm (%.3f°)\n%s', objDist, angleDes, titleStr));
     resolutionFactor = 6;
     tableSize = size(degrees.results_deg)-1;
     
-    plotRange = [0.1, 0.1];                                         % maximum x and y values of the tabular that should be plotted
-    rangeIndizes = ceil(plotRange .* tableSize)+1;
+    % set maximum x and y values of the tabular that should be plotted
+    xlim = 0.1;             % unfortunately, these are the smallest values since the tabular only has 11 entries
+    ylim = 0.1;
+    plotRange = [xlim, ylim];
     
+    % only this part of the tabular in increased in resolution
+    rangeIndizes = ceil(plotRange .* tableSize)+1;
     degreesX = interp2(degrees.results_deg(1:rangeIndizes(1), 1:rangeIndizes(2)), resolutionFactor);
     degSize = size(degreesX);                                       % corresp. to ((rangeIndizes-1)*2^resolutionFactor)+1
+    
+    % now we cut out an even smaller part of this tabular.
+    factor = 8      % 1/factor * axisLim is the part that will be displayed
+    maxIndexX = ceil(degSize(1)/factor);
+    maxIndexY = ceil(degSize(2)/factor);
+    
     scaleSize = ((tableSize(1)-1)*2^resolutionFactor)+1;            % corresp. to the size of the whole tabular with increased resolution
-%     fun = surf(degreesX);                                           % TODO: change axis labels
-    fun = contourf(degreesX(1:ceil(degSize(1)/8), 1:ceil(degSize/8)), 'LineWidth', 0.001);  % temporary solution to enable plotRange = [0.025, 0.025]
-%     alpha(fun, 0.5)                                                 % add transparency to degree plot
+%     fun = surf(degreesX);                                           % TODO: change axis labels 
+%     fun = contourf(linspace(0, xlim / factor, maxIndexX), linspace(0, ylim / factor, maxIndexY), degreesX(1:maxIndexX, 1:maxIndexY), 'LineWidth', 0.001);  % temporary solution to enable plotRange = [0.025, 0.025]
+    fun = contourf(degreesX(1:maxIndexX, 1:maxIndexY), 20, 'LineWidth', 0.001);  % temporary solution to enable plotRange = [0.025, 0.025]
+    colBar = colorbar();
+%     colorbar('Ticks',[-5,-2,1,4,7],...
+%          'TickLabels',{'Cold','Cool','Neutral','Warm','Hot'}) %% todo:
+%          replace vergence degrees with fixation distance
+    colBar.Label.String = 'vergence degree';
+    
+    %     alpha(fun, 0.5)                                                 % add transparency to degree plot
     
     scalingFactor = degSize ./ plotRange;
     for stim = 1:length(stimuli)
@@ -168,6 +225,30 @@ function plotTrajectory(model, objDist, startVergErr, initMethod, numIters, stim
         plot(trajectory(stim,end,1) * scalingFactor + 1, trajectory(stim,end,2) * scalingFactor + 1, 'r.', 'MarkerSize', 50); % , 'MarkerSize', 100
     end
     
+    fun = gca();    
+    fun.XAxis.TickLabels = linspace(0, xlim/factor, maxIndexX); %TODO: get auf jeden Fall noch schoener --> weniger ticks!
+    fun.YAxis.TickLabels = linspace(0, ylim/factor, maxIndexY);
+
+    fun.XTickLabelRotation = -20;
     
-%     plot(trajectory);
+    xlabel('lateral rectus activation [%]');
+    ylabel('medial rectus activation [%]');
+%     fun.XTick = 1 : length(linspace(0, xlim/factor, maxIndexX)); %TODO: get auf jeden Fall noch schoener --> weniger ticks!
+%     fun.YTick = 1 : length(linspace(0, ylim/factor, maxIndexY));
+    
+%     fun.XTickLabel = sprintf('%.4f ', linspace(0, xlim/factor, maxIndexX)); %TODO: get auf jeden Fall noch schoener --> weniger ticks!
+%     fun.YTickLabel = sprintf('%.4f ', linspace(0, ylim/factor, maxIndexY));
+    
+    
+%     xt = get(fun,'xtick');
+% %     xt_label = sprintf('%.2e|',xt);
+%     xt_label = sprintf('%.4f',xt);
+%     set(fun,'xticklabel',xt_label);
+    
+%     yt = get(fun,'ytick');
+% %     yt_label = sprintf('%.2e|',xt);
+%     yt_label = sprintf('%.4f|',yt);
+%     set(fun,'yticklabel',yt_label);
+        
+    
 end
