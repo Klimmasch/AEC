3ffe7dee00e7bd900dc0eb20199d6d31904677ff
Alexander Lelais <lelais@fias.uni-frankfurt.de>
2016-09-08 12:51:34 +0200

    Minor plot update.

--- Model.m
+++ Model.m
@@ -947,7 +947,7 @@ classdef Model < handle
                         cb = colorbar();
                         cb.Label.String = '# Occurences';
 
-                        plotpath = sprintf('%s/muscleGraphsScatterTotalTraining', this.savePath);
+                        plotpath = sprintf('%s/muscleTotalCmdTraining', this.savePath);
                         saveas(gcf, plotpath, 'png');
 
                         % Delta
@@ -973,7 +973,7 @@ classdef Model < handle
                         cb = colorbar();
                         cb.Label.String = '# Occurences';
 
-                        plotpath = sprintf('%s/muscleGraphsScatterDeltaTraining', this.savePath);
+                        plotpath = sprintf('%s/muscleDeltaCmdTraining', this.savePath);
                         saveas(gcf, plotpath, 'png');
                     end
                 end
