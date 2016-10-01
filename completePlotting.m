92e17effd7d7709e1d61c3831721ec8bd3401067
Lukas Klimmasch <klimmasch@fias.uni-frankfurt.de>
2016-10-01 17:36:19 +0200

    Updates in Model and completePlotting

--- completePlotting.m
+++ completePlotting.m
@@ -1,18 +1,20 @@
 % This script takes some files and generated the plots and makes some testing on them
-function completePlotting()
+function completePlotting(simulator)
     files = {...
-        '/home/klimmasch/projects/results/model_17-Sep-2016_09:55:47_5000000_1_bmsf_advancedInit_metCost25/modelAt5000000/model.mat', ...
-        '/home/klimmasch/projects/results/model_20-Sep-2016_17:50:49_5000000_1_bmsf_advancedInit_metCost0_BFActivation5-5/modelAt5000000/model.mat', ...
-        '/home/klimmasch/projects/results/model_17-Sep-2016_09:32:14_5000000_1_bmsf_advancedInit_metCost0/modelAt5000000/model.mat', ...
-        '/home/klimmasch/projects/results/model_13-Sep-2016_17:43:39_6000000_1_bmsf_advancedInit_metCost1/modelAt5500000/model.mat',...
-        '/home/klimmasch/projects/results/model_13-Sep-2016_17:43:39_6000000_1_bmsf_advancedInit_metCost1/modelAt5000000/model.mat',...'
+        '/home/klimmasch/projects/results/model_20-Sep-2016_17:51:44_5000000_1_bmsf_advancedInit_metCost0_criticLR075/modelAt1000000/model.mat', ...
+        '/home/klimmasch/projects/results/model_20-Sep-2016_17:51:44_5000000_1_bmsf_advancedInit_metCost0_criticLR075/modelAt2000000/model.mat', ...
+        '/home/klimmasch/projects/results/model_20-Sep-2016_17:51:44_5000000_1_bmsf_advancedInit_metCost0_criticLR075/modelAt3000000/model.mat', ...
+        '/home/klimmasch/projects/results/model_20-Sep-2016_17:51:44_5000000_1_bmsf_advancedInit_metCost0_criticLR075/modelAt4000000/model.mat', ...
+        '/home/klimmasch/projects/results/model_20-Sep-2016_17:51:44_5000000_1_bmsf_advancedInit_metCost0_criticLR075/modelAt5000000/model.mat', ...
         };
 
     % simulator = OpenEyeSim('create');
     % simulator = OpenEyeSimV4('create');
     % simulator.reinitRenderer();
     % simulator.initRenderer();
-    simulator = prepareSimulator();
+    if isempty(simulator)
+        simulator = prepareSimulator();
+    end
 
     stimulusRange = 40;
     vergRange = 7;
@@ -27,7 +29,7 @@ function completePlotting()
     for i = 1:length(files)
         model = load(files{i});
         model = model.model;
-        model.allPlotSave(6);
+%         model.allPlotSave(6);
         testModelContinuous(model, stimulusRange, 1, 1, simulator, 0, sprintf('modelAt%d', model.trainedUntil));
 
         sprintf('###### plotting and testing completed in %s #######', files{i});
