517ec74127e217acc3eea8c20286339f9916b74b
Alexander Lelais <lelais@fias.uni-frankfurt.de>
2016-10-17 13:46:03 +0200

    Implroved parameter overview plot.

--- plotPerformanceForParameters.m
+++ plotPerformanceForParameters.m
@@ -1,26 +1,26 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% This script reads in a number of models
-%% and plots their performance according  
-%% to a specified set of two paramters.   
+%% and plots their performance according
+%% to a specified set of two paramters.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function plotPerformanceForParamters()
-	files = { ...
-		% '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:35:13_2000000_1_cluster_varDec1e4-1e4/modelAt2000000/model.mat', ...
-  %       '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:40:58_2000000_1_cluster_varDec1e4-1e5/modelAt2000000/model.mat', ...
-  %       '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:41:48_2000000_1_cluster_varDec1e4-1e6/modelAt2000000/model.mat', ...
-  %       '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:48:21_2000000_1_cluster_varDec1e5-1e5/modelAt2000000/model.mat', ...
-  %       '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:49:55_2000000_1_cluster_varDec1e5-1e6/modelAt2000000/model.mat', ...
-  %       '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:51:04_2000000_1_cluster_varDec1e6-1e6/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-2_actorLR_1To0/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-2_actorLR_0.5To0/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-2_actorLR_0.5/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-3_actorLR_1To0/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-3_actorLR_0.5To0/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-3_actorLR_0.5/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-4_actorLR_1To0/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-4_actorLR_0.5To0/modelAt2000000/model.mat', ...
-		'/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-4_actorLR_0.5/modelAt2000000/model.mat', ...
-	};
+function plotPerformanceForParameters()
+    files = { ...
+        '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:35:13_2000000_1_cluster_varDec1e4-1e4/modelAt2000000/model.mat', ...
+        '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:40:58_2000000_1_cluster_varDec1e4-1e5/modelAt2000000/model.mat', ...
+        '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:41:48_2000000_1_cluster_varDec1e4-1e6/modelAt2000000/model.mat', ...
+        '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:48:21_2000000_1_cluster_varDec1e5-1e5/modelAt2000000/model.mat', ...
+        '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:49:55_2000000_1_cluster_varDec1e5-1e6/modelAt2000000/model.mat', ...
+        '/home/aecgroup/aecdata/Results/model_11-Oct-2016_15:51:04_2000000_1_cluster_varDec1e6-1e6/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-2_actorLR_1To0/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-2_actorLR_0.5To0/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-2_actorLR_0.5/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-3_actorLR_1To0/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-3_actorLR_0.5To0/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-3_actorLR_0.5/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-4_actorLR_1To0/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-4_actorLR_0.5To0/modelAt2000000/model.mat', ...
+        % '/home/aecgroup/aecdata/Results/16-10-15_2000000iter_1_regul_1e-4_actorLR_0.5/modelAt2000000/model.mat', ...
+    };
 
 nFiles = length(files);
 
@@ -28,8 +28,8 @@ nFiles = length(files);
 varUpper = [1e-4, 1e-5, 1e-6];
 varLower = [1e-4, 1e-5, 1e-6];
 
-varUpper = [1e-4, 1e-3, 1e-2];
-varLower = [[1, 0], [0.5, 0], [0.5]];
+% varUpper = [1e-4, 1e-3, 1e-2];
+% varLower = [[1, 0], [0.5, 0], [0.5]];
 
 length1 = length(varUpper);
 length2 = length(varLower);
@@ -37,7 +37,7 @@ length2 = length(varLower);
 results = zeros(length1, length2, 3); % results are the three measurements rmse, median, and iqr
 
 for f = 1 : nFiles
-	model = load(files{f});
+    model = load(files{f});
     model = model.model;
 
     % find indizes
@@ -52,8 +52,12 @@ for f = 1 : nFiles
 
 end
 
-%% plotting section 
-plotSavePath = './varianceRangeComparison'
+results(: ,:, 1) = results(:, :, 1)';
+results(: ,:, 2) = results(:, :, 2)';
+results(: ,:, 3) = results(:, :, 3)';
+
+%% plotting section
+plotSavePath = './varianceRangeComparison';
 
 numberFormatVar1 = '%1.0e';
 numberFormatVar2 = '%1.0e';
@@ -69,7 +73,11 @@ title('Parameter Comparison');
 % plot the RMSE
 subplot(3, 1, 1);
 imagesc(results(:, :, 1));
-colormap(jet); %(flipud(autumn)), seems to be the global map for the whole plot
+% colormap(jet); %(flipud(autumn)), seems to be the global map for the whole plot
+
+colordata = createCM(3);
+colordata(1, :) = [1, 1, 1]; % plot NaNs white
+colormap(colordata);
 
 txt = results(:, :, 1);
 txt(find(txt == 0)) = inf;
@@ -79,13 +87,14 @@ txt = strtrim(cellstr(txt));
 [x, y] = meshgrid(1 : max(length1, length2));
 hStrings = text(x(:), y(:), txt(:), 'HorizontalAlignment', 'center');
 
-set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(varUpper(:), numberFormatVar1), ...
+set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(flipud(varUpper(:)), numberFormatVar1), ...
          'YTick', 1:length2, 'YTickLabel', num2str(varLower(:), numberFormatVar2), ...
          'TickLength', [0, 0]);
-     
+
 title('RMSE');
 xlabel(sprintf(labelVar2));
 ylabel(sprintf(labelVar1));
+colorbar();
 
 % saveas(gca, sprintf('%s_rmse.png', plotSavePath));
 
@@ -102,19 +111,20 @@ txt = strtrim(cellstr(txt));
 [x, y] = meshgrid(1 : max(length1, length2));
 hStrings = text(x(:), y(:), txt(:), 'HorizontalAlignment', 'center');
 
-set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(varUpper(:), numberFormatVar1), ...
+set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(flipud(varUpper(:)), numberFormatVar1), ...
          'YTick', 1:length2, 'YTickLabel', num2str(varLower(:), numberFormatVar2), ...
          'TickLength', [0, 0]);
 
 title('IQR*4');
 xlabel(sprintf(labelVar2));
 ylabel(sprintf(labelVar1));
+colorbar();
 
 % saveas(gca, sprintf('%s_iqr.png', plotSavePath));
 
 % plot the median
 subplot(3, 1, 3);
-imagesc(results(:, :, 3));
+imagesc(abs(results(:, :, 3))); % plot absolute values
 % colormap(hsv); %(flipud(autumn))
 
 txt = results(:, :, 3);
@@ -125,15 +135,20 @@ txt = strtrim(cellstr(txt));
 [x, y] = meshgrid(1 : max(length1, length2));
 hStrings = text(x(:), y(:), txt(:), 'HorizontalAlignment', 'center');
 
-set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(varUpper(:), numberFormatVar1), ...
+set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(flipud(varUpper(:)), numberFormatVar1), ...
          'YTick', 1:length2, 'YTickLabel', num2str(varLower(:), numberFormatVar2), ...
          'TickLength', [0, 0]);
-     
+
 title('Median');
 xlabel(sprintf(labelVar2));
 ylabel(sprintf(labelVar1));
+colorbar();
+
+% single colorbar
+% hp4 = get(subplot(3, 1, 3),'Position');
+% colorbar('Position', [hp4(1) + hp4(3) + 0.01  hp4(2)  0.1  hp4(2) + hp4(3) * 2.1])
 
 % saveas(gca, sprintf('%s_median.png', plotSavePath));
 
 saveas(gca, sprintf('%s.png', plotSavePath));
-     
+
