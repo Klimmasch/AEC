8946de8c7f0718c5a2693ff7d57277c31ff50fb4
Lukas Klimmasch <klimmasch@fias.uni-frankfurt.de>
2016-10-22 18:11:36 +0200

    updated number formating in parOES

--- parOES.m
+++ parOES.m
@@ -20,43 +20,38 @@ function parOES(nWorkers)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% parameters that should be explored %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% varNames = {'interval', 'alpha_v'};
-% var1 = {10, 20, 50, 100};
-% var2 = {0.75};
-
-% varNames = {'regularizer', 'actorLRRange'};
-% var1 = {1e-2, 1e-3, 1e-4};
-% var2 = {[0.5], [0.5, 0], [1, 0]};
-
-% varDescr = {'regul', 'actorLR'};
-% var1Descr = {'1e-2', '5e-2', '1e-3', '5e-3', '1e-4'};
-% var2Descr = {'0.5', '0.5To0', '1To0'};
-
 % varNames = {'gamma', 'interval'};
 % var1 = {0.1, 0.3, 0.9};
 % var2 = {10, 50, 100};
 
-varNames = {'criticLRRange', 'actorLRRange'};
-var1 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
-var2 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
-
-%% descriptive parameter names used in folder name
 % varDescr = {'cDiscout', 'interval'};
-% var1Descr = {'01', '03', '09'};
-% var2Descr = {'10', '50', '100'};
+% numberFormatVar1 = '%1.1f';
+% numberFormatVar2 = '2.0f';
 
-varDescr = {'CriticLR', 'ActorLR'};
-var1Descr = {'[1,1]', '[1,0]', '[0.75,0.75]', '[0.75,0]', '[0.5,0.5]', '[0.5,0]', '[0.25,0.25]', '[0.25,0]'};
-var2Descr = {'[1,1]', '[1,0]', '[0.75,0.75]', '[0.75,0]', '[0.5,0.5]', '[0.5,0]', '[0.25,0.25]', '[0.25,0]'};
+varNames = {'regularizer', 'actorLRRange'};
+var1 = {5e-4, 5e-5, 1e-5, 1e-6};    % {1e-2, 1e-3, 1e-4};
+var2 = {[0.5, 0], [1, 0]};
+
+varDescr = {'regul', 'actorLR'};
+numberFormatVar1 = '%1.0e';
+numberFormatVar2 = '[%1.2f-%1.2f]';
+
+% varNames = {'criticLRRange', 'actorLRRange'};
+% var1 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
+% var2 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
+
+% varDescr = {'CriticLR', 'ActorLR'};
+% numberFormatVar1 = '[%1.2f-%1.2f]';
+% numberFormatVar2 = '[%1.2f-%1.2f]';
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% general parameter section %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 nIters = 2000000;               % number of iterations
 rSeed = 1;                      % random seed
-folderName = '';
+% folderName = '';
 % folderName = 'Regularizer vs Actor Learning Rate' % no ';' intended.
-% folderName = 'Discount Factor vs Interval' % no ';' intended.
+folderName = 'Discount vs Interval' % no ';' intended.
 %TODO enable definition of other parameters that are not changed.
 % standardParams = {'textureFile', textureFiles, 'trainTime', trainTime, 'testAt', testAt, 'sparseCodingType', sparseCodingType};
 
@@ -65,15 +60,6 @@ folderName = '';
 %%%%%%% staring here, the rest is done automatically and should - in gerneral - not be altered  %%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-myCluster = parcluster('local');
-myCluster.NumWorkers = nWorkers;
-% saveProfile(myCluster);
-
-% create a parallel worker pool if there is none
-if (isempty(gcp('nocreate')))
-    mPool = parpool(nWorkers);
-end
-
 nParams = length(var1) * length(var2);
 paramValues = cell(nParams, 2); % column vector, each row contains a set of params for one run
 
@@ -81,9 +67,23 @@ iter = 1;
 for i = 1 : length(var1)
     for j = 1 : length(var2)
         paramValues(iter, :) = {cell2mat(var1(i)), cell2mat(var2(j))};
-        paramStrings(iter, :) = {cell2mat(var1Descr(i)), cell2mat(var2Descr(j))};
+        paramStrings(iter, :) = {num2str(var1{i}, numberFormatVar1), num2str(var2{j}, numberFormatVar2)};
         iter = iter + 1;
     end
+end
+
+%% if no other descriptors are given, take original values
+if isempty(varDescr)
+    varDescr = varNames;
+end
+
+myCluster = parcluster('local');
+myCluster.NumWorkers = nWorkers;
+% saveProfile(myCluster);
+
+% create a parallel worker pool if there is none
+if (isempty(gcp('nocreate')))
+    mPool = parpool(nWorkers);
 end
 
 %% small test
@@ -95,26 +95,13 @@ end
 %
 % results
 
-%% if no other descriptors are given, take original values
-if isempty(varDescr)
-    varDescr = varNames;
-end
-
-if isempty(var1Descr)
-    var1Descr = var1;
-end
-
-if isempty(var2Descr)
-    var2Descr = var2;
-end
-
 % textureFiles = {'Textures_mcgillManMade40.mat', 'Textures_mcgillManMade100.mat'};
 % textureFiles = {'mcGillTest2.mat', 'mcGillTest1.mat'}; % test files containing less images
 % simulator = prepareSimulator(textureFiles); % at first, we try to use a shared simulator for all threads
 
 %% main loop
 parfor ind = 1 : nParams
-    sprintf('%s=[%4.2f,%4.2f], %s=[%4.2f,%4.2f]', varNames{1}, paramValues{ind, 1}(1), paramValues{ind, 1}(2), varNames{2}, paramValues{ind, 2}(1), paramValues{ind, 2}(2))
+    % sprintf('%s=[%4.2f,%4.2f], %s=[%4.2f,%4.2f]', varNames{1}, paramValues{ind, 1}(1), paramValues{ind, 1}(2), varNames{2}, paramValues{ind, 2}(1), paramValues{ind, 2}(2))
 
     % OES2Muscles(nIters, rSeed, 1, paramValues(ind), sprintf('%s%s_%s%s', varDescr{1}, paramStrings{ind, 1}, varDescr{2}, paramStrings{ind, 2})); % sprintf('varDec%g--%g', paramValues{ind})
 
