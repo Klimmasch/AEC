2d51c85bbf72f33bf8286e3fba33ecc11fa96365
Alexander Lelais <lelais@fias.uni-frankfurt.de>
2016-11-03 17:11:13 +0100

    - added new colormap - added two critic value function overview plots for cluster runs

--- plotPerformanceForParameters.m
+++ plotPerformanceForParameters.m
@@ -62,14 +62,14 @@ function plotPerformanceForParameters(modelAt)
     numberFormatVar1 = '[%1.2f - %1.2f]';
     numberFormatVar2 = '[%1.2f - %1.2f]';
 
-    plotSavePath = strcat(parentFolder, '/CriticLRActorLR.png');
+    plotSavePath = strcat(parentFolder, '/CriticLRActorLR');
 
     %%%%%%% after this, you just have to update the extraction of values in the for loop %%%%%%%%%%%
 
     length1 = length(var1);
     length2 = length(var2);
 
-    results = zeros(length1, length2, 3); % results are the three measurements rmse, median, and iqr
+    results = zeros(length1, length2, 5); % results are the three measurements rmse, median, and iqr, critValDelta, critValNiveau
     subFolder = sprintf('modelAt%d', modelAt);
 
     % iter = 1;
@@ -108,14 +108,19 @@ function plotPerformanceForParameters(modelAt)
             results(ind, jnd, 2) = iqr(model.testResult3(:, testInterval)) * 4;
             results(ind, jnd, 3) = median(model.testResult3(:, testInterval));
 
-            % results(ceil(f / 3) , iter, 1) = sqrt(mean(model.testResult3(:, testInterval) .^ 2));
-            % results(ceil(f / 3) , iter, 2) = iqr(model.testResult3(:, testInterval));
-            % results(ceil(f / 3) , iter, 3) = median(model.testResult3(:, testInterval));
+            %%% Critic value function steepness
+            % critValDelta = mean(critic_value(vergErr = 0) - (critic_value(vergErr = -0.5) + critic_value(vergErr = 0.5)) / 2)
+            % critValNiveau = mean(critic_value(vergErr = 0))
+            % average over all stimuli and each objDist
+            test2Resolution = 101;
+            vseRange = linspace(-1, 1, test2Resolution);
 
-            % iter = iter + 1;
-            % if iter > 3
-            %     iter = 1;
-            % end
+            % critValDelta
+            results(ind, jnd, 4) = mean(mean(abs(abs(model.testResult4(:, vseRange == 0, 1 : 2 + length(model.scModel) : end)) ...
+                                               - abs((model.testResult4(:, vseRange == -0.5, 1 : 2 + length(model.scModel) : end) ...
+                                                     + model.testResult4(:, vseRange == 0.5, 1 : 2 + length(model.scModel) : end)) / 2)), 3));
+            % critValNiveau
+            results(ind, jnd, 5) = mean(mean(abs(model.testResult4(:, vseRange == 0, 1 : 2 + length(model.scModel) : end)), 3));
         catch
            % catch case when (sub-)experiment started, but has no test results yet
            continue;
@@ -240,4 +245,75 @@ function plotPerformanceForParameters(modelAt)
     % saveas(gca, sprintf('%s_median.png', plotSavePath));
 
     saveas(gca, sprintf('%s_at%diter.png', plotSavePath, modelAt));
+
+    %%% Critic's value function
+    colordata = createCM(3);
+    colordata = flipud(colordata);
+    if (~isempty(results(results == 0)))
+        colordata(1, :) = [1, 1, 1];
+    end
+
+    figure;
+    % suptitle(sprintf('Parameter Comparison at %d iterations', modelAt));
+
+    % plot the critValDelta
+    subplot(2, 1, 1);
+    colormap(colordata);
+    imagesc(results(:, :, 4));
+
+    txt = results(:, :, 4);
+    txt(txt == 0) = Inf;
+    txt = num2str(txt(:),'%0.3f');
+    txt = strtrim(cellstr(txt));
+    [x, y] = meshgrid(1 : max(length1, length2));
+    hStrings = text(x(:), y(:), txt(:), 'HorizontalAlignment', 'center');
+
+    % one dim. case
+    % set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(flip(var2(:)), numberFormatVar1), ...
+    %          'YTick', 1:length2, 'YTickLabel', num2str(var1(:), numberFormatVar2), ...
+    %          'TickLength', [0, 0]);
+    % two dim case
+    set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(var1(:, :), numberFormatVar1), ...
+             'YTick', 1:length2, 'YTickLabel', num2str(var2(:, :), numberFormatVar2), ...
+             'TickLength', [0, 0], 'FontSize', 7);
+    % set(gca, 'XTick', 1:length1, 'XTickLabel', var1, ...
+    %          'YTick', 1:length2, 'YTickLabel', var2, ...
+    %          'TickLength', [0, 0]);
+
+    title(strcat('\Deltacritic_{val}', sprintf(' = |mean(critic_{val}(verg_{Err} = 0)\n - (critic_{val}(verg_{Err} = -0.5) + critic_{val}(verg_{Err} = 0.5)) / 2)|')));
+    xlabel(sprintf(labelVar1));
+    ylabel(sprintf(labelVar2));
+    colorbar();
+
+    % plot the critValNiveau
+    subplot(2, 1, 2);
+    colormap(colordata);
+    imagesc(results(:, :, 5));
+
+    txt = results(:, :, 5);
+    txt(txt == 0) = Inf;
+    txt = num2str(txt(:),'%0.3f');
+    % txt(find(txt == '0.00')) = ' '; % this may cause errors.
+    txt = strtrim(cellstr(txt));
+    [x, y] = meshgrid(1 : max(length1, length2));
+    hStrings = text(x(:), y(:), txt(:), 'HorizontalAlignment', 'center');
+
+    % one dim. case
+    % set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(flip(var2(:)), numberFormatVar1), ...
+    %          'YTick', 1:length2, 'YTickLabel', num2str(var1(:), numberFormatVar2), ...
+    %          'TickLength', [0, 0]);
+    % two dim case
+    set(gca, 'XTick', 1:length1, 'XTickLabel', num2str(var1(:, :), numberFormatVar1), ...
+             'YTick', 1:length2, 'YTickLabel', num2str(var2(:, :), numberFormatVar2), ...
+             'TickLength', [0, 0], 'FontSize', 7);
+    % set(gca, 'XTick', 1:length1, 'XTickLabel', var1, ...
+    %          'YTick', 1:length2, 'YTickLabel', var2, ...
+    %          'TickLength', [0, 0]);
+
+    title(sprintf('critic_{val} Niveau = |mean(critic_{val}(verg_{Err} = 0))|'));
+    xlabel(sprintf(labelVar1));
+    ylabel(sprintf(labelVar2));
+    colorbar();
+
+    saveas(gca, sprintf('%s_CriticVal_at%diter.png', plotSavePath, modelAt));
 end
