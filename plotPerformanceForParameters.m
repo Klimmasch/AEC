e2ca983ad9b56d13a9ce20d9557e2abcfe2cb701
Lukas Klimmasch <klimmasch@fias.uni-frankfurt.de>
2017-01-20 12:58:07 +0100

    extended plotPerformanceForParameters with a plot that displays the step length of motor commands

--- plotPerformanceForParameters.m
+++ plotPerformanceForParameters.m
@@ -16,64 +16,128 @@ function plotPerformanceForParameters(modelAt)
     % 8)    figure name
 
     %%% actor LR vs critic LR
-    parentFolder = '/home/aecgroup/aecdata/Results/CriticLR vs ActorLR';
-
-    labelVar1 = 'actor learning range';
-    labelVar2 = 'critic learning range';
-
-    var1 = [[1, 1]; [1, 0]; [0.75, 0.75]; [0.75, 0]; [0.5, 0.5]; [0.5, 0]; [0.25, 0.25]; [0.25, 0]];
-    var2 = [[1, 1]; [1, 0]; [0.75, 0.75]; [0.75, 0]; [0.5, 0.5]; [0.5, 0]; [0.25, 0.25]; [0.25, 0]];
-    var1 = flipud(var1); % setting the offspring to lower left hand corner
-
-    numberFormatVar1 = '[%1.2f - %1.2f]';
-    numberFormatVar2 = '[%1.2f - %1.2f]';
-
-    figName = 'CriticLRActorLR';
+%     parentFolder = '/home/aecgroup/aecdata/Results/CriticLR vs ActorLR';
+% 
+%     labelVar1 = 'actor learning range';
+%     labelVar2 = 'critic learning range';
+% 
+%     var1 = [[1, 1]; [1, 0]; [0.75, 0.75]; [0.75, 0]; [0.5, 0.5]; [0.5, 0]; [0.25, 0.25]; [0.25, 0]];
+%     var2 = [[1, 1]; [1, 0]; [0.75, 0.75]; [0.75, 0]; [0.5, 0.5]; [0.5, 0]; [0.25, 0.25]; [0.25, 0]];
+%     var1 = flipud(var1); % setting the offspring to lower left hand corner
+% 
+%     numberFormatVar1 = '[%1.2f - %1.2f]';
+%     numberFormatVar2 = '[%1.2f - %1.2f]';
+% 
+%     figName = 'CriticLRActorLR';
 
 
     %%% regularizer vs actor leaning range
-    % parentFolder = '/home/aecgroup/aecdata/Results/Regularizer vs Actor Learning Rate';
-
-    labelVar1 = 'Actor weight regularizer';
-    labelVar2 = 'Actor LR [start, end]';
-
-    var1 = [1e-2; 1e-3; 5e-4; 1e-4; 5e-5; 1e-5; 1e-6]; % regularizer
-    var2 = [[1, 0]; [0.5, 0]; [0.5, 0.5]]; % actorLearningRange
-
-    numberFormatVar1 = '%1.0e';
-    numberFormatVar2 = '[%1.2f - %1.2f]';
-
-
-    % figName = 'hiddenLayerRegulActorLRComparison';
+%     parentFolder = '/home/aecgroup/aecdata/Results/Regularizer vs Actor Learning Rate';
+% 
+%     labelVar1 = 'Actor weight regularizer';
+%     labelVar2 = 'Actor LR [start, end]';
+% 
+%     var1 = [1e-2; 1e-3; 5e-4; 1e-4; 5e-5; 1e-5; 1e-6]; % regularizer
+%     var2 = [[1, 0]; [0.5, 0]; [0.5, 0.5]]; % actorLearningRange
+% 
+%     numberFormatVar1 = '%1.0e';
+%     numberFormatVar2 = '[%1.2f - %1.2f]';
+% 
+% 
+%     figName = 'hiddenLayerRegulActorLRComparison';
 
     %%% discount factor vs interval
-    % parentFolder = '/home/aecgroup/aecdata/Results/Gamma_vs_Interval_fewerResources';
-
-    % labelVar1 = 'discount factor';
-    % labelVar2 = 'interval';
-
-    % var1 = [0.1; 0.3; 0.9];
-    % var2 = [10; 50; 100];
-
-    % numberFormatVar1 = '%1.1f';
-    % numberFormatVar2 = '%d';
-
-    % figName = 'DiscountVsInterval';
+%     parentFolder = '/home/aecgroup/aecdata/Results/Gamma_vs_Interval_fewerResources';
+% 
+%     labelVar1 = 'discount factor';
+%     labelVar2 = 'interval';
+% 
+%     var1 = [0.1; 0.3; 0.9];
+%     var2 = [10; 50; 100];
+% 
+%     numberFormatVar1 = '%1.1f';
+%     numberFormatVar2 = '%d';
+% 
+%     figName = 'DiscountVsInterval';
 
     %%% metabolic costs
-    % parentFolder = '/home/klimmasch/projects/results/exploringMetCost';
+%     parentFolder = '/home/klimmasch/projects/results/exploringMetCost';
+% 
+% 
+%     labelVar1 = 'metabolic Costs';
+%     labelVar2 = '';
+% 
+%     var1 = [[0.0324, 0.0324]; [0.0162, 0.0162]; [0.0121, 0.0121]; [0.0081, 0.0081]; [0.0040, 0.0040]; [0,0]];
+%     var2 = [1e-4]; % only chooses values with the right regularizer
+% 
+%     numberFormatVar1 = '[%1.4f,%1.4f]';
+%     numberFormatVar2 = '%';
+% 
+%     figName = 'metCostComp';
+
+    %%% new variance decay
+%     parentFolder = '/home/aecgroup/aecdata/Results/varDec_new';
+% 
+%     labelVar1 = 'critic learning range';
+%     labelVar2 = 'variance range';
+% 
+%     var1 = [[1, 0]; [0.75, 0.75]];
+%     var2 = [[1e-4, 5e-6]; [1e-4, 0]; [5e-5, 5e-5]; [5e-5, 5e-6]; [5e-5, 0]; [1e-5, 1e-5]; [1e-5, 5e-6]; [1e-5, 0]; [5e-6, 5e-6]; [5e-6, 5e-6]; [5e-6, 0]];
+%     % var1 = flipud(var1); % setting the offspring to lower left hand corner
+% 
+%     numberFormatVar1 = '[%1.2f - %1.2f]';
+%     numberFormatVar2 = '[%1.0e - %1.0e]';
+% 
+%     figName = 'varDecVsCritic';
+
+    %%% steplength: actor LR vs regularizer (new)
+    % parentFolder = '/home/aecgroup/aecdata/Results/steplength_actorVsRegul';
 
 
-    % labelVar1 = 'metabolic Costs';
-    % labelVar2 = '';
+    %  labelVar1 = 'Actor weight regularizer';
+    % labelVar2 = 'Actor LR [start, end]';
 
-    % var1 = [[0.0324, 0.0324]; [0.0162, 0.0162]; [0.0121, 0.0121]; [0.0081, 0.0081]; [0.0040, 0.0040]; [0,0]];
-    % var2 = [1e-4]; % only chooses values with the right regularizer
+    % var1 = [1e-4; 1e-5; 1e-6]; % regularizer
+    % var2 = [[1, 1]; [0.5, 0.5]; [1, 0]; [0.5, 0]]; % actorLearningRange
 
-    % numberFormatVar1 = '[%1.4f,%1.4f]';
-    % numberFormatVar2 = '%';
+    % numberFormatVar1 = '%1.0e';
+    % numberFormatVar2 = '[%1.2f - %1.2f]';
 
-    % figName = 'metCostComp';
+
+    % figName = 'steplength_actorLRvsRegul';
+
+    %% steplength: actor LR vs variance range (regularizar: 1e-5 to increase stepwith further)
+    parentFolder = '/home/aecgroup/aecdata/Results/steplength_actorVsVariance_reg1e-5';
+
+
+    labelVar1 = 'Actor LR [start, end]';
+    labelVar2 = 'Variance Range  [start, end]';
+
+    var1 = [[1, 1]; [0.5, 0.5]; [1, 0]; [0.5, 0]];
+    var2 = [[1e-4, 1e-4]; [1e-5, 1e-5]; [1e-4, 1e-6]; [1e-5, 1e-6]; [1e-4, 0]; [1e-5, 0]];
+
+    numberFormatVar1 = '[%1.1f-%1.1f]';
+    numberFormatVar2 = '[%1.0e-%1.0e]';
+
+
+    figName = 'steplength_actorLRvsVar_reg1e-5';
+
+    %% actor LR vs Weight initialization
+%     parentFolder = '/home/aecgroup/aecdata/Results/steplength_actorVsWeightInit';
+% 
+% 
+%     labelVar1 = 'Actor LR [start, end]';
+%     labelVar2 = 'Weight Init [crit, hid, out]';
+% 
+%     var1 = [[1, 1]; [0.5, 0.5]; [1, 0]; [0.5, 0]];
+%     var2 = [[1e-3, 6e-5, 2e-2]; [1e-2, 6e-5, 2e-2]; [1e-3, 1e-4, 2e-2]; [1e-3, 6e-5, 1e-1]; [1e-3, 1e-4, 1e-1]; [1e-2, 1e-4, 1e-1]];
+% 
+%     numberFormatVar1 = '[%1.1f-%1.1f]';
+%     numberFormatVar2 = '[%1.0e,%1.0e,%1.0e]';
+% 
+% 
+%     figName = 'steplength_actorVsWeights';
+
 
     % ------------------
     % Results extraction
@@ -100,8 +164,9 @@ function plotPerformanceForParameters(modelAt)
     % results := |var1| x |var2| x
     %            {rmse(vergErr), median(vergErr), and iqr(vergErr),
     %             rmse(deltaMC), median(deltaMC), and iqr(deltaMC),
-    %             critValDelta, critValNiveau}
-    results = zeros(length1, length2, 8);
+    %             critValDelta, critValNiveau,
+    %             stepLengthFirst, stepLengthTotal}    %see below for explanation of this values 
+    results = zeros(length2, length1, 10);
 
     for f = 1 : length(subExperiments)
         try
@@ -116,10 +181,10 @@ function plotPerformanceForParameters(modelAt)
         % testInterval = 200;
 
         % hack for some older simulations:
-        % if length(model.rlModel.actorLearningRange) == 1
-        %     value = model.rlModel.actorLearningRange;
-        %     model.rlModel.actorLearningRange = [value, value];
-        % end
+        if length(model.rlModel.actorLearningRange) == 1
+            value = model.rlModel.actorLearningRange;
+            model.rlModel.actorLearningRange = [value, value];
+        end
 
         %%% Extract indicies
         %%% actor exploration variance range
@@ -130,21 +195,39 @@ function plotPerformanceForParameters(modelAt)
         % ind = find(ismember(var2, model.rlModel.actorLearningRange, 'rows'));
         % jnd = find(ismember(var1, model.rlModel.CActor.regularizer, 'rows'));
 
-        %%% actor LR vs critic LR
-        ind = find(ismember(var2, model.rlModel.criticLearningRange, 'rows'));
-        jnd = find(ismember(var1, model.rlModel.actorLearningRange, 'rows'));
+        % %%% actor LR vs critic LR
+%         ind = find(ismember(var2, model.rlModel.criticLearningRange, 'rows'));
+%         jnd = find(ismember(var1, model.rlModel.actorLearningRange, 'rows'));
 
         %%% discount factor vs interval
-        % ind = find(ismember(var2, model.interval, 'rows'));
-        % jnd = find(ismember(var1, model.rlModel.CCritic.gamma, 'rows'));
+%         ind = find(ismember(var2, model.interval, 'rows'));
+%         jnd = find(ismember(var1, model.rlModel.CCritic.gamma, 'rows'));
 
         %%% regularizer vs metabolic costs
-        % ind = find(ismember(var2, model.rlModel.CActor.regularizer, 'rows'));
-        % jnd = find(ismember(var1, model.metCostRange, 'rows'));
+%         ind = find(ismember(var2, model.rlModel.CActor.regularizer, 'rows'));
+%         jnd = find(ismember(var1, model.metCostRange, 'rows'));
+
+        %%% actor LR vs critic LR
+%         ind = find(ismember(var2, model.rlModel.CActor.varianceRange, 'rows'));
+%         jnd = find(ismember(var1, model.rlModel.criticLearningRange, 'rows'));
+
+        %%% actor LR vs variance range
+        ind = find(ismember(var2, model.rlModel.CActor.varianceRange, 'rows'));
+        jnd = find(ismember(var1, model.rlModel.actorLearningRange, 'rows'));
+
+        %%% actor LR vs weight init
+%         ind = find(ismember(var2, model.rlModel.weight_range, 'rows'));
+%         jnd = find(ismember(var1, model.rlModel.actorLearningRange, 'rows'));
 
         %%% Extract results
         %%% Vergence Error
-        results(ind, jnd, 1) = sqrt(mean(model.testResult3(:, testInterval) .^ 2));
+        try
+            results(ind, jnd, 1) = sqrt(mean(model.testResult3(:, testInterval) .^ 2));
+        catch
+            display(model.savePath)
+            sprintf('seems to lack the training results ...')
+            continue
+        end
         results(ind, jnd, 2) = iqr(model.testResult3(:, testInterval)) * 4;
         results(ind, jnd, 3) = median(model.testResult3(:, testInterval));
 
@@ -164,6 +247,15 @@ function plotPerformanceForParameters(modelAt)
                                                  + model.testResult4(:, vseRange == 0.5, 1 : 2 + length(model.scModel) : end)) / 2)), 3));
         % critValNiveau
         results(ind, jnd, 8) = mean(mean(abs(model.testResult4(:, vseRange == 0, 1 : 2 + length(model.scModel) : end)), 3));
+        
+        %%% investigate step width: take absolute difference between consecutive
+        %%% muscle activities or vergence errors as measurement; better:
+        %%% absolute relative muscle activities! model.testResult5[n](abs(1), abs(2), rel(1), rel(2))
+        %%% For every model, display first value for 0.5 and 6 meters, and the sum over all 20 trials
+        startInd = find(mod(1 : length(model.testResult5), testInterval) == 1); %indizes of every first action during testing
+        % results(ind, jnd, 9) = norm([model.testResult5(startInd, 3), model.testResult5(startInd, 4)], 2);
+        results(ind, jnd, 9) = sum(sqrt(sum(model.testResult5(startInd, 3:4)'.^2)));
+        results(ind, jnd, 10) = sum(sqrt(sum(model.testResult5(:, 3:4)'.^2)));
     end
 
     % ----------------
@@ -178,10 +270,10 @@ function plotPerformanceForParameters(modelAt)
 
     [x, y] = meshgrid(1 : length1, 1 : length2);
     titleStrings = {'RMSE', 'IQR*4', 'Median'};
-    resultsIter = {[1 : 3]; [4 : 6]; [7, 8]};
+    resultsIter = {[1 : 3]; [4 : 6]; [9, 10]; [7, 8]};
 
-    for figIter = 1 : 3
-        if (figIter == 3)
+    for figIter = 1 : 4
+        if (figIter >= 3)
             colordata = createCM(3);
             colordata = flipud(colordata);
             if (~isempty(results(results == 0)))
@@ -202,7 +294,7 @@ function plotPerformanceForParameters(modelAt)
 
             txt = results(:, :, resultsIter{figIter}(subfigIter));
             txt(txt == 0) = Inf;
-            if (figIter < 3)
+            if (figIter < 4)
                 txt = num2str(txt(:), '%0.2f');
             else
                 txt = num2str(txt(:), '%0.3f');
@@ -210,27 +302,31 @@ function plotPerformanceForParameters(modelAt)
             txt = strtrim(cellstr(txt));
             text(x(:), y(:), txt(:), 'HorizontalAlignment', 'center');
 
-            if (size(var1, 2) == 2)
+%             if (size(var1, 2) == 2)
                 % two dim case
                 set(gca, 'XTick', 1 : length1, 'XTickLabel', num2str(var1(:, :), numberFormatVar1), ...
                          'YTick', 1 : length2, 'YTickLabel', num2str(var2(:, :), numberFormatVar2), ...
                          'TickLength', [0, 0], 'FontSize', 7);
-            elseif (size(var1, 2) == 1)
-                % one dim. case
-                set(gca, 'XTick', 1 : length1, 'XTickLabel', var1, ...
-                         'YTick', 1 : length2, 'YTickLabel', var2, ...
-                         'TickLength', [0, 0]);
-            else
-                error('Parameter range var1 has unsupported size of %d x %d.', size(var1));
-            end
+%             elseif (size(var1, 2) == 1)
+%                 % one dim. case
+%                 set(gca, 'XTick', 1 : length1, 'XTickLabel', var1, ...
+%                          'YTick', 1 : length2, 'YTickLabel', var2, ...
+%                          'TickLength', [0, 0]);
+%             else
+%                 error('Parameter range var1 has unsupported size of %d x %d.', size(var1));
+%             end
 
             if (figIter < 3)
                 title(titleStrings{subfigIter});
-            elseif (subfigIter == 1)
+            elseif (figIter == 3 && subfigIter == 1)
+                title('First Step Length');
+            elseif (figIter == 3 && subfigIter == 2)
+                title('Total Step Length');
+            elseif (figIter == 4 && subfigIter == 1)
                 title(strcat('\Deltacritic_{val}', ...
                              sprintf(' = |mean(critic_{val}(verg_{Err} = 0)\n - (critic_{val}(verg_{Err} = -0.5)'), ...
                              ' + critic_{val}(verg_{Err} = 0.5)) / 2)|'));
-            else
+            else  (figIter == 4 && subfigIter == 2)
                 title(sprintf('critic_{val} Niveau = |mean(critic_{val}(verg_{Err} = 0))|'));
             end
 
@@ -241,12 +337,17 @@ function plotPerformanceForParameters(modelAt)
 
 
         if (figIter == 1)
-            % suptitle(sprintf('Parameter Comparison at %d iterations', modelAt));
-            saveas(gca, sprintf('%s_at%diter.png', plotSavePath, modelAt));
+            suptitle(sprintf('Vergence Error at %d iterations', modelAt));
+            saveas(gca, sprintf('%s_vergErr_at%diter.png', plotSavePath, modelAt));
         elseif (figIter == 2)
             suptitle(sprintf('Metabolic Costs at %d iterations', modelAt));
             saveas(gca, sprintf('%s_metCost_at%diter.png', plotSavePath, modelAt));
         elseif (figIter == 3)
+            suptitle(sprintf('Step Length at %d iterations', modelAt));
+            saveas(gca, sprintf('%s_StepLength_at%diter.png', plotSavePath, modelAt));
+        elseif (figIter == 4)
             saveas(gca, sprintf('%s_CriticVal_at%diter.png', plotSavePath, modelAt));
         end
     end
+
+    
