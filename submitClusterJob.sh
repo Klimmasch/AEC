66b2538a06430745c6d2f40fabab3e6ebefe5968
Alexander Lelais <lelais@fias.uni-frankfurt.de>
2017-03-02 17:18:49 +0100

    Tidied up cluster call script.

--- submitClusterJob.sh
+++ submitClusterJob.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# usage: submitClusterJob.sh [WORKERS]
+
 # check if #workers were provided
 if [ $# -eq 0 ]
 then
@@ -8,18 +10,20 @@ then
     WORKERS=$1
 fi
 
-#SBATCH -p sleuth -c 8 --mem-per-cpu 3072 --gres gpu:4 -LXserver
-#following parameters are set experimentally thinking of a parallel job with 9 threads (-c 18 --mem-per-cpu=2048)
+# standard cluster call
+srun --partition=sleuths --cpus-per-task=$WORKERS --mem-per-cpu=5120 --gres gpu:2 -LXserver \
+--job-name=GammaVsMetCosts \
+matlab -nodisplay -r "parOES(${WORKERS}); quit"
 
-#the time command should track the cpu usage
-# srun -p sleuths -c 18 --mem-per-cpu 2560 --gres gpu -LXserver time matlab -nodisplay -r "parOES; quit" #the time command should track the cpu usage
+# the time command can track the cpu usage
+# srun --partition=sleuths --cpus-per-task=$WORKERS --mem-per-cpu=5120 --gres gpu:2 -LXserver \
+# time matlab -nodisplay -r "parOES(1); quit"
 
 # possible outcome of the time command:
 # 191.01user 30.59system 35:04:55elapsed 0%CPU (0avgtext+0avgdata 515960maxresident)k
 # 14424inputs+92808outputs (5major+1873105minor)pagefaults 0swaps
 
-# --test-only
-# --mem-per-cpu=2560
-srun --partition=sleuths --cpus-per-task=$WORKERS --mem-per-cpu=5120 --gres gpu:2 -LXserver \
---job-name=CriticLRActorLR \
-matlab -nodisplay -r "parOES(${WORKERS}); quit"
+# alternative cluster call (batch job)
+# SBATCH --partition=sleuths --cpus-per-task=$WORKERS --mem-per-cpu=5120 --gres gpu:2 -LXserver\
+# --job-name=GammaVsMetCosts \
+# matlab -nodisplay -r "parOES(${WORKERS}); quit"
