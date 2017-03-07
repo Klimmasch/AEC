c03c673b53dc7465ad285be8a7b2ffa7e336c93d
Alexander Lelais <lelais@fias.uni-frankfurt.de>
2017-03-07 20:06:42 +0100

    cleaned up parOES.

--- parOES.m
+++ parOES.m
@@ -23,45 +23,73 @@ function parOES(nWorkers)
 % varNames = {'gamma', 'interval'};
 % var1 = {0.1, 0.3, 0.9};
 % var2 = {10, 50, 100};
-
 % varDescr = {'cDiscout', 'interval'};
 % numberFormatVar1 = '%1.1f';
 % numberFormatVar2 = '%d';
-
 % experimentDirName = 'Discount Factor vs Interval' % no ';' intended.
 
-% varNames = {'regularizer', 'actorLRRange'};
-% var1 = {5e-4, 5e-5, 1e-5, 1e-6};    % {1e-2, 1e-3, 1e-4};
-% var2 = {[0.5, 0], [1, 0]};
+% varNames = {'gamma', 'metCostRange'};
+% var1 = {0.1, 0.3, 0.6, 0.9};
+% var2 = {[0], [0.01], [0.05], [0.1]};
+% varDescr = {'gamma', 'metCost'};
+% numberFormatVar1 = '%1.1f';
+% numberFormatVar2 = '[%1.2f]';
+% experimentDirName = 'GammaVsMetCostsBiasFineGrain'
 
+% varNames = {'gamma', 'metCostRange'};
+% var1 = {0.01, 0.07, 0.1, 0.2};
+% var2 = {[0], [0.0017], [0.0033], [0.005]};
+% varDescr = {'gamma', 'metCost'};
+% numberFormatVar1 = '%1.2f';
+% numberFormatVar2 = '[%1.4f]';
+% experimentDirName = 'GammaVsMetCostsBiasFineGrain'
+
+% varNames = {'actorLRRange', 'varianceRange'};
+% var1 = {[1,1], [0.5,0.5], [1, 0], [0.5, 0]};
+% var2 = {[5e-5,5e-5], [5e-5,0], [1e-5,1e-5], [1e-5,0]};
+% varDescr = {'actorLRRange', 'varianceRange'};
+% numberFormatVar1 = '[%1.1f-%1.1f]';
+% numberFormatVar2 = '[%1.0e-%1.0e]';
+% experimentDirName = 'actorLR_vs_varRange_norm_feat_mio' % no ';' intended.
+
+% varNames = {'regularizer', 'actorLRRange'};
+% var1 = {1e-4, 1e-5, 1e-6};    % {1e-2, 1e-3, 1e-4};
+% var2 = {[1,1], [0.5,0.5], [1, 0], [0.5, 0]};
 % varDescr = {'regul', 'actorLR'};
 % numberFormatVar1 = '%1.0e';
 % numberFormatVar2 = '[%1.2f-%1.2f]';
+% experimentDirName = 'actorLR_vs_regul_norm_feat' % no ';' intended.
 
-% experimentDirName = 'Regularizer vs Actor Learning Rate' % no ';' intended.
+% varNames = {'criticLRRange', 'actorLRRange'};
+% var1 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
+% var2 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
+% var1Descr = {'[1,1]', '[1,0]', '[0.75,0.75]', '[0.75,0]', '[0.5,0.5]', '[0.5,0]', '[0.25,0.25]', '[0.25,0]'};
+% var2Descr = {'[1,1]', '[1,0]', '[0.75,0.75]', '[0.75,0]', '[0.5,0.5]', '[0.5,0]', '[0.25,0.25]', '[0.25,0]'};
+% varDescr = {'CriticLR', 'ActorLR'};
+% numberFormatVar1 = '[%1.2f-%1.2f]';
+% numberFormatVar2 = '[%1.2f-%1.2f]';
+% experimentDirName = 'CriticLR vs ActorLR';
 
+% varNames = {'regularizer', 'desiredStdZT'};
+% var1 = {5e-5, 1e-5, 5e-6, 1e-6};
+% var2 = {0.005, 0.0075, 0.01, 0.015, 0.02, 0.025};
+% var1Descr = {'5e-5', '1e-5', '5e-6', '1e-6'};
+% var2Descr = {'0.005', '0.0075', '0.01', '0.015', '0.02', '0.025'};
+% varDescr = {'regularizer', 'desiredStdZT'};
+% experimentDirName = 'Regularizer_vs_desiredStdZT_short';
 
-varNames = {'criticLRRange', 'actorLRRange'};
-var1 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
-var2 = {[1, 1], [1, 0], [0.75, 0.75], [0.75, 0], [0.5, 0.5], [0.5, 0], [0.25, 0.25], [0.25, 0]};
-
-var1Descr = {'[1,1]', '[1,0]', '[0.75,0.75]', '[0.75,0]', '[0.5,0.5]', '[0.5,0]', '[0.25,0.25]', '[0.25,0]'};
-var2Descr = {'[1,1]', '[1,0]', '[0.75,0.75]', '[0.75,0]', '[0.5,0.5]', '[0.5,0]', '[0.25,0.25]', '[0.25,0]'};
-
-varDescr = {'CriticLR', 'ActorLR'};
-numberFormatVar1 = '[%1.2f-%1.2f]';
-numberFormatVar2 = '[%1.2f-%1.2f]';
-
-experimentDirName = 'CriticLR vs ActorLR';
+% varNames = {'lambdaMuscleFB', 'desiredStdZT'};
+% var1 = {0, 0.5, 1, 2};
+% var2 = {0.005, 0.0075, 0.01, 0.015, 0.02};
+% var1Descr = {'0', '0.5', '1', '2'};
+% var2Descr = {'0.005', '0.0075', '0.01', '0.015', '0.02'};
+% varDescr = {'lambdaMuscleFB', 'desiredStdZT'};
+% experimentDirName = 'lambdaMuscleFB_vs_desiredStdZT_seed2';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% general parameter section %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-nIters = 2000000;               % number of iterations
+nIters = 1000000;               % number of iterations
 rSeed = 1;                      % random seed
-
-%TODO enable definition of other parameters that are not changed.
-% standardParams = {'textureFile', textureFiles, 'trainTime', trainTime, 'testAt', testAt, 'sparseCodingType', sparseCodingType};
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%% staring here, the rest is done automatically and should - in gerneral - not be altered  %%%%%%%
@@ -74,8 +102,7 @@ iter = 1;
 for i = 1 : length(var1)
     for j = 1 : length(var2)
         paramValues(iter, :) = {cell2mat(var1(i)), cell2mat(var2(j))};
-        paramStrings(iter, :) = {cell2mat(var1Descr(i)), cell2mat(var2Descr(j))}; % temporary solution for not renaming all folders ...
-        % paramStrings(iter, :) = {num2str(var1{i}, numberFormatVar1), num2str(var2{j}, numberFormatVar2)};
+        paramStrings(iter, :) = {num2str(var1{i}, numberFormatVar1), num2str(var2{j}, numberFormatVar2)};
         iter = iter + 1;
     end
 end
@@ -94,15 +121,6 @@ if (isempty(gcp('nocreate')))
     mPool = parpool(nWorkers);
 end
 
-%% small test
-% results = zeros(2, nParams);
-%
-% parfor (ind = 1 : nParams)
-%   results(:, ind) = params(:, ind)
-% end
-%
-% results
-
 % textureFiles = {'Textures_mcgillManMade40.mat', 'Textures_mcgillManMade100.mat'};
 % textureFiles = {'mcGillTest2.mat', 'mcGillTest1.mat'}; % test files containing less images
 % simulator = prepareSimulator(textureFiles); % at first, we try to use a shared simulator for all threads
@@ -110,12 +128,6 @@ end
 %% main loop
 parfor ind = 1 : nParams
     % sprintf('%s=[%4.2f,%4.2f], %s=[%4.2f,%4.2f]', varNames{1}, paramValues{ind, 1}(1), paramValues{ind, 1}(2), varNames{2}, paramValues{ind, 2}(1), paramValues{ind, 2}(2))
-
-    % OES2Muscles(nIters, rSeed, 1, paramValues(ind), sprintf('%s%s_%s%s', varDescr{1}, paramStrings{ind, 1}, varDescr{2}, paramStrings{ind, 2})); % sprintf('varDec%g--%g', paramValues{ind})
-
-    % OES2Muscles(nIters, rSeed, 1, ...
-    %             {varNames{1}, paramValues{ind, 1}, varNames{2}, paramValues{ind, 2}}, ...
-    %             sprintf('cluster_%s_%4.2f_%s_%.0f', varDescr{1}, paramValues{ind, 1}, varDescr{2}, paramValues{ind, 2}));
 
     sprintf('%s_%s_%s_%s', varDescr{1}, paramStrings{ind, 1}, varDescr{2}, paramStrings{ind, 2})
     OES2Muscles(nIters, rSeed, 1, ...
