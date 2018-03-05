a16b7fefe495fcb6554e8f9373e497ea14cad8c5
Lukas Klimmasch <klimmasch@fias.uni-frankfurt.de>
2018-03-05 14:17:24 +0100

    added the script that generates the distortion filters for the eyes

---
+++ orientedGaussian.m
@@ -0,0 +1,7 @@
+function g = orientedGaussian(fSize, sigma1, sigma2)
+    ind = -floor(fSize/2) : floor(fSize/2);
+    g1 = exp(-(ind.^2) / (2*sigma1));
+    g2 = exp(-(ind.^2) / (2*sigma2));
+    g = g1' * g2;
+    g = g / sum(g(:));
+end
